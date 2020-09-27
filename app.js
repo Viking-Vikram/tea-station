54aab3c62d14b1253466668d5d5fa1d3fc31b83d
Viking-Vikram <kadarlavikky@gmail.com>
2020-09-28 02:10:59 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//setup date
+const date = (document.getElementById(
+ "date"
+).innerHTML = new Date().getFullYear());
+// setup nav
+const navBtn = document.getElementById("nav-btn");
+const navbar = document.getElementById("navbar");
+const navClose = document.getElementById("nav-close");
+// close nav
+navClose.addEventListener("click", () => {
+ navbar.classList.remove("showNav");
+});
+// show nav
+navBtn.addEventListener("click", () => {
+ navbar.classList.add("showNav");
+});
